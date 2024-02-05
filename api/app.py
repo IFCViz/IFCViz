7588ba3b8f458f438df93396156abab997caca37
monoamine11231 <monoamine11231@gmail.com>
2024-02-05 21:00:44 +0100

    In dev backend upload file

--- api/app.py
+++ api/app.py
@@ -1,7 +1,25 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, file
+
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+ERROR_HTML: str = '<h1>ERROR MESSAGE WAS RETURNED</h1>'
+
+def secure_filename(filename: str) -> str:
+    return "".join(x for x in filename if x.isalpha())
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    if 'file' not in request.files:
+        flash('No file part included')
+        return ERROR_HTML
+
+    if file.filename == '':
+        flash('No IFC file selected')
+        return ERROR_HTML
+
+    filename = secure_filename(filename)
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    
