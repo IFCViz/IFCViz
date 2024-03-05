79b989319623261d7302356c486f7be7c6e786cd
monoamine11231 <monoamine11231@gmail.com>
2024-03-05 16:30:57 +0100

    Upload tests for IFC file validity and ifc metadata api endpoint implemented

--- api/app.py
+++ api/app.py
@@ -10,6 +10,11 @@ from flask import Flask
 from parser import parse
 import db
 
+import ifcopenshell
+import ifcopenshell.file
+import ifcopenshell.validate
+
+
 app = Flask(__name__)
 app.secret_key = "super secret key"
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -58,9 +63,10 @@ def send(filehash: str):
 
 @app.route("/upload", methods=['POST'])
 def upload():
-    # ERROR_NO_GZIP = make_response(json.dumps({'error': 'file not gzipped'}),        400)
+    ERROR_NO_GZIP = make_response(json.dumps({'error': 'file not gzipped'}),        400)
     ERROR_NO_SAVE = make_response(json.dumps({'error': 'file cannot be saved'}),    500)
     ERROR_F_EXIST = make_response(json.dumps({'error': 'file already exists'}),     500)
+    ERROR_INVALID = make_response(json.dumps({'error': 'file is not ifc'}),         400)
 
     # KEEP THE CODE BELLOW IF SWITCHING TO FORM FILE UPLOAD STYLE!!!!!
     # ================================================================
@@ -74,12 +80,21 @@ def upload():
 
     contents: bytes = request.data
 
-    # gzip_file: gzip.GzipFile = gzip.GzipFile(fileobj=io.BytesIO(request.data), mode='rb')
-    # gzip_contents: Optional[bytes] = try_or_default(None)(gzip_file.read)()
-    # gzip_file.close()
-    #
-    # if not gzip_contents:
-    #     return ERROR_NO_GZIP
+    gzip_file: gzip.GzipFile = gzip.GzipFile(fileobj=io.BytesIO(request.data), mode='rb')
+    gzip_contents: Optional[bytes] = try_or_default(None)(gzip_file.read)()
+    gzip_file.close()
+    
+    if not gzip_contents:
+        return ERROR_NO_GZIP
+    
+    model_reader_safe = try_or_default(None)(ifcopenshell.file().from_string)
+    model: Optional[ifcopenshell.file] = model_reader_safe(gzip_contents.decode())
+    logger: Optional[ifcopenshell.validate.json_logger] \
+        = ifcopenshell.validate.json_logger()
+    error: Optional[int] = try_or_default(-1)(ifcopenshell.validate.validate)(
+        model,logger)
+    if error == -1:
+        return ERROR_INVALID
 
     filename: str = sha256(contents).hexdigest()
     # If implementing hash storage in DB use query instead of `os.path.isfile``
@@ -92,11 +107,23 @@ def upload():
     return make_response(json.dumps({'fileid': filename}), 200)
 
 
-@app.route("/", methods=["GET"])
-def parsing_result():
-    # Returns a json
-    return parse(app.config['UPLOAD_FOLDER'] + 'simple_house.ifc')
-    # return render_template("../webapp/template/index.html")
+@app.route("/metadata/<string:filehash>")
+def metadata(filehash: str):
+    ERROR_NO_FILE = make_response(json.dumps({'error': 'file does not exist'}),     400)
+
+    filename: str = secure_filename(filehash)
+    # If implementing hash storage in DB use query instead of `os.path.isfile``
+    if not os.path.isfile(app.config['UPLOAD_FOLDER']+filename):
+        return ERROR_NO_FILE
+    
+    return parse(app.config['UPLOAD_FOLDER'] + filename)
+
+
+# @app.route("/", methods=["GET"])
+# def parsing_result():
+#     # Returns a json
+#     return parse(app.config['UPLOAD_FOLDER'] + 'simple_house.ifc')
+#     # return render_template("../webapp/template/index.html")
 
 
 if __name__ == '__main__':
