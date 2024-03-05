7eaa67ced352707205accc0b9866cf0bb082b3f6
antmag <antmag@kth.se>
2024-03-05 13:30:02 +0100

    Concluding merge, will do more changes later. To hook up dima's branch with frontend.

---
+++ README.md
@@ -0,0 +1,31 @@
+# IFCViz
+A tool to visualize IFC files in a browser
+
+## Dependencies
+- Flask:
+```bash
+pip3 install flask
+pip3 install ifcopenshell
+pip3 install lark
+```
+- Node.js (>=18.0.0)
+- NPM
+- yarn:
+```bash
+sudo npm install --global yarn
+```
+
+## Submodules
+Fetch them using:
+```bash
+git submodule update --init
+```
+
+## How to run the server?
+Start the Flask backend server:
+```bash
+cd api/ && flask run
+```
+
+## Docs
+[IFC.js](https://docs.thatopen.com/Tutorials/FragmentIfcLoader)
---
+++ api/app.py
@@ -0,0 +1,106 @@
+import os
+import io
+import json
+import gzip
+from hashlib import sha256
+from flask import Flask, request, make_response, send_file
+from typing import Optional
+
+from flask import Flask
+from parser import parse
+
+app = Flask(__name__)
+app.secret_key = "super secret key"
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+
+
+def secure_filename(filename: str) -> str:
+    return "".join(x for x in filename if x.isalnum())
+
+
+# Function wrapper, returns `default` if exception rises in `f`. Must have
+# ..since Flask ignores try/except and raises the catched error
+def try_or_default(default):
+    def wrap(f):
+        def inner(*args, **kwargs):
+            try:
+                return f(*args, **kwargs)
+            except Exception as e:
+                return default
+        return inner
+    return wrap
+
+
+# Helper to prevent try/catch inside Flask app
+def dump_contents(contents: bytes, filename: str) -> bool:
+    with open(app.config['UPLOAD_FOLDER']+filename, 'wb') as f:
+        f.write(contents)
+
+    return True
+
+
+@app.route("/receive/<string:filehash>")
+def send(filehash: str):
+    ERROR_NO_FILE = make_response(json.dumps({'error': 'file does not exist'}),     400)
+
+    filename: str = secure_filename(filehash)
+    # If implementing hash storage in DB use query instead of `os.path.isfile``
+    if not os.path.isfile(app.config['UPLOAD_FOLDER']+filename):
+        return ERROR_NO_FILE
+
+    f = open(app.config['UPLOAD_FOLDER']+filename, 'rb')
+    contents: io.BytesIO = io.BytesIO(f.read())
+    f.close()
+
+    return send_file(contents, mimetype='application/gzip', as_attachment=False)
+
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    ERROR_NO_GZIP = make_response(json.dumps({'error': 'file not gzipped'}),        400)
+    ERROR_NO_SAVE = make_response(json.dumps({'error': 'file cannot be saved'}),    500)
+    ERROR_F_EXIST = make_response(json.dumps({'error': 'file already exists'}),     500)
+
+    # KEEP THE CODE BELLOW IF SWITCHING TO FORM FILE UPLOAD STYLE!!!!!
+    # ================================================================
+    # if 'file' not in request.files or request.files[0].filename == '':
+    #     flash('No file part included')
+    #
+    #     return ERROR_NO_FILE
+    #
+    # file = request.files['file']
+    # ================================================================
+
+    contents: bytes = request.data
+
+    gzip_file: gzip.GzipFile = gzip.GzipFile(fileobj=io.BytesIO(request.data), mode='rb')
+    gzip_contents: Optional[bytes] = try_or_default(None)(gzip_file.read)()
+    gzip_file.close()
+
+    if not gzip_contents:
+        return ERROR_NO_GZIP
+
+    filename: str = sha256(contents).hexdigest()
+    # If implementing hash storage in DB use query instead of `os.path.isfile``
+    if os.path.isfile(app.config['UPLOAD_FOLDER']+filename):
+        return ERROR_F_EXIST
+
+    if not try_or_default(False)(dump_contents)(contents, filename):
+        return ERROR_NO_SAVE
+
+    return make_response(json.dumps({'fileid': filename}), 200)
+
+
+@app.route("/", methods=["GET"])
+def welcome():
+    return "Hello World!"
+
+
+@app.route("/test")
+def hello_world():
+    jsn = parse('../test_files/simple_house.ifc')
+    return f"<p>{jsn}</p>"
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=False)
