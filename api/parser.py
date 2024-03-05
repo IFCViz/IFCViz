71cc5967032f267e1a3d4f2a18bceba6a758b698
monoamine11231 <monoamine11231@gmail.com>
2024-03-05 16:29:55 +0100

    ifcopenshell parsing fixed to work with gzipped files

--- api/parser.py
+++ api/parser.py
@@ -1,13 +1,19 @@
 import ifcopenshell
+import ifcopenshell.file
 import ifcopenshell.util.element
 import json
+import gzip
 
 
 def parse(file_name):
-    model = ifcopenshell.open(file_name)
+    ERROR_NO_FLOORS = json.dumps({"error": "no floors found!"})
+
+    model = None
+    with gzip.open(file_name) as gz:
+        model = ifcopenshell.file().from_string(gz.read().decode())
 
     # Get model name
-    model_name = file_name.split("/")[-1]
+    model_name = "Some model name"
     # Make the name cleaner
     # model_name = model_name.split(".")[0].replace("_", " ").replace("-", " ").title()
     # print(model_name)
@@ -15,7 +21,7 @@ def parse(file_name):
 
     floors = [floor for floor in model.by_type('IfcSlab') if ifcopenshell.util.element.get_predefined_type(floor) == "FLOOR"]
     if len(floors) == 0:
-        return "No floors found!<br>"
+        return ERROR_NO_FLOORS
 
     # result = f"Amount of floor type objects: {len(floors)}<br><br>"
 
@@ -27,9 +33,5 @@ def parse(file_name):
         json_dict[model_name]["floors"].append({floor.Name: base_properties['GrossArea']})
         # result += f"Object name: {floor.Name}<br>"
         # result += f"&emsp;&emsp;Area: {base_properties['GrossArea']} m^2<br>"
-    final_json = json.dumps(json_dict)
-    return final_json
-
-
-if __name__ == "__main__":
-    parse('uploads/simple_house.ifc')
+    
+    return json.dumps(json_dict)
