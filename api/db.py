c939c1087f68719cc3621b8626a4b50246bb46be
monoamine11231 <monoamine11231@gmail.com>
2024-04-22 14:21:09 +0200

    fix(db): Moved to VARCHAR for hash key storage

--- api/db.py
+++ api/db.py
@@ -1,39 +1,51 @@
 import psycopg
+from typing import Optional
 
-def format_hash(hash: str) -> str:
-    return "0x" + str(hash)[10]
 
 def get_conn():
     return psycopg.connect("dbname=postgres user=postgres host=localhost port=5432")
 
-def new_analysis(filehash, file_content, parsed):
-    conn = get_conn()
-    conn.cursor().execute("INSERT INTO analysis (id, ifc_file, parsed) VALUES (%s, %s, %s)", (format_hash(filehash), file_content, parsed))
-    conn.commit()
-    conn.close()
+def new_analysis(filehash, file_content, parsed) -> bool:
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
 
-def get_analysis(filehash: str):
+        q: str = "INSERT INTO analysis (id, ifc_file, parsed) VALUES (%s, %s, %s)"
+        cur.execute(q, (filehash, file_content, parsed))
+        conn.commit()
+        conn.close()
+        return True
+    except:
+        return False
+
+def get_analysis(filehash: str) -> Optional[psycopg.rows.TupleRow]:
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM analysis WHERE id=%s", (filehash,))
+        res = cur.fetchone()
+        conn.close()
+        return res
+    except:
+        return None
+
+
+def get_metadata(filehash: str) -> Optional[str]:
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM analysis WHERE id=%s", (filehash,))
+        res = cur.fetchone()[2]
+        conn.close()
+        return res
+    except:
+        return None
+
+
+def get_file(filehash: str) -> Optional[bytes]:
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
-    res = cur.fetchone()
-    conn.close()
-    return res
-
-
-def get_metadata(filehash: str) -> str:
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
-    res = cur.fetchone()[2]
-    conn.close()
-    return res
-
-
-def get_file(filehash: str) -> bytes:
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
+    cur.execute("SELECT * FROM analysis WHERE id=%s", (filehash,))
     res = cur.fetchone()[1]
     conn.close()
     return res
@@ -41,13 +53,16 @@ def get_file(filehash: str) -> bytes:
 def delete_analysis(filehash: str):
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute("DELETE FROM analysis WHERE id=%s", (format_hash(filehash),))
+    cur.execute("DELETE FROM analysis WHERE id=%s", (filehash,))
     conn.close()
 
 def analysis_exists(filehash: str) -> bool:
-    conn = get_conn()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
-    res = cur.fetchone()
-    conn.close()
-    return res != None
+    try:
+        conn = get_conn()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM analysis WHERE id=%s", (filehash,))
+        res = cur.fetchone()
+        conn.close()
+        return res != None
+    except:
+        return False
--- database/schema/schema.sql
+++ database/schema/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE analysis (
-	id BIGINT PRIMARY KEY,
+	id VARCHAR(64) PRIMARY KEY,
 	ifc_file BYTEA,
 	parsed TEXT
 );
