714f6869ed79c4fafaa9a7efccf33a0b6e264c02
monoamine11231 <monoamine11231>
2024-04-22 12:00:15 +0200

    feat(db): Method implemented for checking if an analysis exists based on the generated SHA256 filehash

--- api/db.py
+++ api/db.py
@@ -32,9 +32,15 @@ def get_metadata(filehash):
 def get_file(filehash):
     conn = get_conn()
     cur = conn.cursor()
-    conn.cursor().execute("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
+    cur.execute("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
     res = cur.fetchone()[1]
     conn.close()
     return res
 
-
+def analysis_exists(filehash: str) -> bool:
+    conn = get_conn()
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
+    res = cur.fetchone()
+    conn.close()
+    return res != None
