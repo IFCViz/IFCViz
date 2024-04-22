0597f50fc6d6894f7cb15fd2a5df02eaf9e0430e
monoamine11231 <monoamine11231>
2024-04-22 10:35:55 +0200

    feat(tests): Major tests implemented for the upload/ endpoint

--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -1,3 +1,4 @@
+import gzip
 import json
 from api.app import app, secure_filename
 
@@ -10,8 +11,27 @@ def test_secure_filename():
 
 def test_upload():
     res_empty = app.test_client().post('/upload')
-    assert res_empty.data == json.dumps({'error': 'file not gzipped'}).encode('ASCII')
+    assert res_empty.data == json.dumps(
+        {'error': 'no content provided'}
+    ).encode('ASCII')
     assert res_empty.status_code == 400
+
+    res_not_gzip = app.test_client().post('/upload', data=b'not in gzip format')
+    assert res_not_gzip.data == json.dumps(
+        {'error': 'file not gzipped'}
+    ).encode("ASCII")
+    assert res_not_gzip.status_code == 400
+
+    res_is_gzip_not_ifc = app.test_client().post(
+        '/upload',
+        data=gzip.compress(b'not ifc')
+    )
+    assert res_is_gzip_not_ifc.data == json.dumps(
+        {'error': 'file is not ifc'}
+    ).encode('ASCII')
+    assert res_is_gzip_not_ifc.status_code == 400
+
+    
 
     # ================================================================================
 
