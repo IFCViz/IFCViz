0bf533e344d8c6f9c4026ba8045476e556333a84
monoamine11231 <monoamine11231@gmail.com>
2024-04-22 14:37:18 +0200

    fix(db): General typos and bugs fixed

--- api/db.py
+++ api/db.py
@@ -1,38 +1,42 @@
 import psycopg
+from psycopg.rows import TupleRow
 from typing import Optional
 
 
 def get_conn():
     return psycopg.connect("dbname=postgres user=postgres host=localhost port=5432")
 
-def query_one_row(query_str, tuple):
+def query_one_row(query: str, tuple: tuple, result: bool = True) -> Optional[TupleRow]:
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
-    res = cur.fetchone()
+    cur.execute(query, tuple)
+    res: Optional[TupleRow] = None
+    if result:
+        res = cur.fetchone()
     conn.commit()
     conn.close()
     return res
 
 
-def new_analysis(filehash, file_content, parsed):
-    query_one_row.execute("INSERT INTO analysis (id, ifc_file, parsed) VALUES (%s, %s, %s)", (format_hash(filehash), file_content, parsed))
+def new_analysis(filehash: str, file_content: bytes, parsed: str):
+    q: str = "INSERT INTO analysis (id, ifc_file, parsed) VALUES (%s, %s, %s)"
+    query_one_row(q, (filehash, file_content, parsed), False)
 
 
 def get_analysis(filehash):
-    return query_one_row("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
+    return query_one_row("SELECT * FROM analysis WHERE id=%s", (filehash,), False)
 
 
 def get_metadata(filehash):
-    return query_one_row("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))[2]
+    return query_one_row("SELECT * FROM analysis WHERE id=%s", (filehash,), False)[2]
 
 
 def get_file(filehash):
-    return query_one_row("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))[1]
+    return query_one_row("SELECT * FROM analysis WHERE id=%s", (filehash,))[1]
 
 def delete_analysis(filehash: str):
-    return query_one_row("DELETE FROM analysis WHERE id=%s", (format_hash(filehash),))
+    return query_one_row("DELETE FROM analysis WHERE id=%s", (filehash,), False)
 
 def analysis_exists(filehash: str) -> bool:
-    res = query_one_row("SELECT * FROM analysis WHERE id=%s", (format_hash(filehash),))
+    res = query_one_row("SELECT * FROM analysis WHERE id=%s", (filehash,))
     return res != None
