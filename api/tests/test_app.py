e4259a8df7fe42583e02b05f3af51b9402827bd6
monoamine11231 <monoamine11231>
2024-04-22 13:13:46 +0200

    feat(tests): Completed tests implemented for upload/ endpoint

--- api/tests/test_app.py
+++ api/tests/test_app.py
@@ -2,6 +2,7 @@ import gzip
 import json
 import re
 from api.app import app, secure_filename
+from api.db import delete_analysis
 
 from typing import Optional
 
@@ -35,20 +36,32 @@ def test_upload():
     assert res_is_gzip_not_ifc.status_code == 400
 
     # ================================================================================
+    rsha256hash = re.compile(r"^[a-fA-F0-9]{64}$")
+
     ifc_sample: Optional[bytes] = None
     with open('../ifc_test_files/simple_house.ifc', 'rb') as f:
         ifc_sample = f.read()
 
-    res_is_gzip_is_ifc = app.test_client().post(
+    # Upload two times the same file
+    res_is_gzip_is_ifc_1 = app.test_client().post(
         '/upload',
         data=gzip.compress(ifc_sample)
     )
-    resha256hash = re.compile(r"^[a-fA-F0-9]{64}$")
-    assert res_is_gzip_is_ifc.status == 200
+    assert res_is_gzip_is_ifc_1.status_code == 200
 
-    res_is_gzip_is_ifc_res_json = json.loads(res_is_gzip_is_ifc.data.decode('ASCII'))
-    assert resha256hash.fullmatch(res_is_gzip_is_ifc_res_json.get('fileid', '')) != None
+    res_is_gzip_is_ifc_1_res_json = json.loads(res_is_gzip_is_ifc_1.data.decode('ASCII'))
+    assert rsha256hash.fullmatch(res_is_gzip_is_ifc_1_res_json.get('fileid', '')) != None
 
+    res_is_gzip_is_ifc_2 = app.test_client().post(
+        '/upload',
+        data=gzip.compress(ifc_sample)
+    )
+    assert res_is_gzip_is_ifc_2.status_code == 200
+
+    res_is_gzip_is_ifc_2_res_json = json.loads(res_is_gzip_is_ifc_2.data.decode('ASCII'))
+    assert rsha256hash.fullmatch(res_is_gzip_is_ifc_2_res_json.get('fileid', '')) != None
+
+    delete_analysis(res_is_gzip_is_ifc_2_res_json['fileid'])
 
 def test_send():
     pass
